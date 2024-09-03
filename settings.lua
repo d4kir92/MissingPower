adb6d7b64d5085ea2db2ccce044981bc082674ac
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-09-03 09:18:32 +0200

    v1.2.12

--- settings.lua
+++ settings.lua
@@ -14,14 +14,14 @@ end
 
 function MissingPower:InitSetting()
 	MIPOPC = MIPOPC or {}
-	MissingPower:SetVersion(AddonName, 136048, "1.2.11")
+	MissingPower:SetVersion(AddonName, 136048, "1.2.12")
 	mp_settings = MissingPower:CreateFrame(
 		{
 			["name"] = "MissingPower",
 			["pTab"] = {"CENTER"},
 			["sw"] = 520,
 			["sh"] = 520,
-			["title"] = format("MissingPower |T136048:16:16:0:0|t v|cff3FC7EB%s", "1.2.11")
+			["title"] = format("MissingPower |T136048:16:16:0:0|t v|cff3FC7EB%s", "1.2.12")
 		}
 	)
 
@@ -222,7 +222,7 @@ function MissingPower:InitSetting()
 					["name"] = "MissingPower",
 					["icon"] = 136048,
 					["dbtab"] = MIPOPC,
-					["vTT"] = {{"MissingPower |T136048:16:16:0:0|t", "v|cff3FC7EB1.2.11"}, {"Leftclick", "Toggle Settings"}, {"Rightclick", "Hide Minimap Icon"}},
+					["vTT"] = {{"MissingPower |T136048:16:16:0:0|t", "v|cff3FC7EB1.2.12"}, {"Leftclick", "Toggle Settings"}, {"Rightclick", "Hide Minimap Icon"}},
 					["funcL"] = function()
 						MissingPower:ToggleSettings()
 					end,
