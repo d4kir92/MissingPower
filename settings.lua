a31aae8c76c3f56145500d7efdf6c0ca90f8feed
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-01-25 10:30:54 +0100

    v1.2.59

--- settings.lua
+++ settings.lua
@@ -13,7 +13,7 @@ end
 
 function MissingPower:InitSetting()
 	MIPOPC = MIPOPC or {}
-	MissingPower:SetVersion(136048, "1.2.58")
+	MissingPower:SetVersion(136048, "1.2.59")
 	mp_settings = MissingPower:CreateWindow(
 		{
 			["name"] = "MissingPower",
